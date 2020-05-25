5ce9b662c676acbddc157799273ba719f7496311
Sandro Pernerstorfer <sandro.pernerstorfer@hotmail.com>
2020-05-25 10:23:22 +0200

    added basic html and js

---
+++ basic.js
@@ -0,0 +1,9 @@
+console.log('Hello my name is Sandro');
+
+
+var age = 'and i am 24 years old!';
+console.log(age);
+
+
+var players = ['Martin','Thomas','Peter','Mathias','Niki'];
+console.log('The most valuable player of the match is '+players[2]);
